f74a7a7eb109fe5dea775891dd976847214d3041
LI <g1478963252008@gmail.com>
2025-04-18 15:29:19 +0800

    修改json格式內容，新增customYears

--- BackendAPI/Utils/JsonSanitizer.cs
+++ BackendAPI/Utils/JsonSanitizer.cs
@@ -9,35 +9,30 @@ namespace BackendAPI.Utils
             using var doc = JsonDocument.Parse(jsonString);
             var root = doc.RootElement;
 
-            var events = root.TryGetProperty("events", out var eventsProp) && eventsProp.ValueKind == JsonValueKind.Array
-                 ? eventsProp.EnumerateArray().Select(e => (object)new
-                 {
-                     id = e.TryGetProperty("id", out var idProp) && idProp.ValueKind == JsonValueKind.String
-                         ? idProp.GetString()
-                         : Guid.NewGuid().ToString(),
-                     year = e.GetProperty("year").GetInt32(),
-                     month = e.GetProperty("month").ToString(),
-                     day = e.GetProperty("day").ToString(),
-                     title = e.GetProperty("title").GetString(),
-                     content = e.GetProperty("content").GetString(),
-                     eraName = e.GetProperty("eraName").GetString(),
-                     tags = e.GetProperty("tags").EnumerateArray().Select(t => t.GetString()).ToArray()
-                 }).ToList()
-                 : new List<object>();
+            var jsonDict = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString)!;
 
-
-            var eras = root.TryGetProperty("eras", out var erasProp) && erasProp.ValueKind == JsonValueKind.Array
-                ? erasProp
-                : JsonDocument.Parse("[]").RootElement;
-
-            var updated = new
+            if (root.TryGetProperty("events", out var eventsProp) && eventsProp.ValueKind == JsonValueKind.Array)
             {
-                events,
-                eras
-            };
+                var eventsWithIds = eventsProp.EnumerateArray().Select(e => new Dictionary<string, object>
+                {
+                    ["id"] = e.TryGetProperty("id", out var idProp) && idProp.ValueKind == JsonValueKind.String
+                        ? idProp.GetString()
+                        : Guid.NewGuid().ToString(),
+                    ["year"] = e.GetProperty("year").GetInt32(),
+                    ["month"] = e.GetProperty("month").ToString(),
+                    ["day"] = e.GetProperty("day").ToString(),
+                    ["title"] = e.GetProperty("title").GetString() ?? "",
+                    ["content"] = e.GetProperty("content").GetString() ?? "",
+                    ["eraName"] = e.GetProperty("eraName").GetString() ?? "",
+                    ["tags"] = e.GetProperty("tags").EnumerateArray().Select(t => t.GetString()).ToArray()
+                }).ToList();
 
-            return JsonSerializer.Serialize(updated);
+                jsonDict["events"] = eventsWithIds;
+            }
+
+            return JsonSerializer.Serialize(jsonDict);
         }
+
     }
 
 }
